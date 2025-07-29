bf8ebdc47e3e620dfcf54f493e9d4fd3bb30745c
AudeNec <aude.nec@gmail.com>
2025-07-29 11:19:28 +0200

    first init

---
+++ server/src/config/db.ts
@@ -0,0 +1,11 @@
+import { DataSource } from "typeorm";
+
+const dataSource = new DataSource({
+  type: "postgres",
+  database: "db_footsy",
+  entities: [],
+  synchronize: true,
+  logging: ["error", "query"],
+});
+
+export default dataSource;
---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import { buildSchema } from "type-graphql";
+import dataSource from "./config/db";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import "reflect-metadata";
+
+const port = Number.parseInt(process.env.PORT) ?? 4000;
+
+async function startServer() {
+  await dataSource.initialize();
+  const schema = await buildSchema({
+    resolvers: [],
+  });
+  const apolloServer = new ApolloServer({ schema });
+  const { url } = await startStandaloneServer(apolloServer, {
+    listen: { port },
+  });
+  console.info(`Server started on ${url}`);
+}
+startServer();
