3f3e61da514be88b717f759e757bfc8a8b6297d0
verocussi <verocussi@gmail.com>
2025-07-29 15:06:50 +0200

    Update server + docker + bdd

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,7 +2,11 @@ import { DataSource } from "typeorm";
 
 const dataSource = new DataSource({
   type: "postgres",
-  database: "db_footsy",
+  host: process.env.DB_HOST || "localhost",
+  port: 5432,
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "password",
+  database: process.env.DB_DATABASE || "footsy_db",
   entities: [],
   synchronize: true,
   logging: ["error", "query"],
