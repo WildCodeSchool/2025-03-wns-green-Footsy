e608fe14070614f4577eaf27f2d7d143805db970
AudeNec <aude.nec@gmail.com>
2025-09-11 16:41:57 +0200

    feat: add avatars

---
+++ server/src/seeders/data/AvatarSeeder.ts
@@ -0,0 +1,54 @@
+const Avatars = [
+  {
+    id: 1,
+    image: "icon-greta.png",
+    title: "Greta",
+  },
+  {
+    id: 2,
+    image: "icon-hugo-clement.png",
+    title: "Hugo Clément",
+  },
+  {
+    id: 3,
+    image: "icon-jane-goodall.png",
+    title: "Jane Goodall",
+  },
+  {
+    id: 4,
+    image: "icon-lorax.png",
+    title: "Lorax",
+  },
+  {
+    id: 5,
+    image: "icon-mononoke.png",
+    title: "Mononoke",
+  },
+  {
+    id: 6,
+    image: "icon-paul-watson.png",
+    title: "Paul Watson",
+  },
+  {
+    id: 7,
+    image: "icon-pocahontas.png",
+    title: "Pocahontas",
+  },
+  {
+    id: 8,
+    image: "icon-totoro.png",
+    title: "Totoro",
+  },
+  {
+    id: 9,
+    image: "icon-wall.png",
+    title: "Wall-e",
+  },
+  {
+    id: 10,
+    image: "icon-wangari.png",
+    title: "Wangari Maathai",
+  },
+];
+
+export { Avatars };
