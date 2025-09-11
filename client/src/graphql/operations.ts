84bc05c601dc356685f020162c233a58b6ed465d
Foubert Antoine <ant.foub@gmail.com>
2025-09-11 15:12:59 +0200

    Revert "Feat/apollo client (#19)"
    This reverts commit 2c6df08a4b2f34492c4cefdcb7db530a745bf1e8.

--- client/src/graphql/operations.ts
+++
@@ -1,7 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const SIGN_UP = gql`
-  mutation SignUp($data: NewUserInput!) {
-    signup(data: $data)
-  }
-`;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,3 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { ApolloProvider } from "@apollo/client/react";
 import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
@@ -14,7 +12,6 @@ import TestCharte from "./pages/testsCharte/TestsCharte";
 
 import "./reset.css";
 import "./index.css";
-import { HttpLink } from "@apollo/client";
 
 const router = createBrowserRouter([
   {
@@ -45,19 +42,10 @@ if (rootElement == null) {
   throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
 }
 
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: import.meta.env.VITE_URL_GRAPHQL || "http://localhost:5050/",
-  }),
-  cache: new InMemoryCache(),
-});
-
 createRoot(rootElement).render(
   <StrictMode>
     <ModeProvider>
-      <ApolloProvider client={client}>
-        <RouterProvider router={router} />
-      </ApolloProvider>
+      <RouterProvider router={router} />
     </ModeProvider>
   </StrictMode>
 );
