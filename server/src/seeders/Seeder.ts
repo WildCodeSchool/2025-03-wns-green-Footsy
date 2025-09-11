7bd542e3e0172c9357bfb57376747039cd0f72ff
AdrienSergent <adrien.sergent@hotmail.fr>
2025-09-11 10:25:11 +0200

    fix seeder

--- server/src/seeders/Seeder.ts
+++ server/src/seeders/Seeder.ts
@@ -1,13 +1,18 @@
 import dataSource from "../config/db";
 import { Users } from "./data/UserSeeder";
+import { Avatars } from "./data/AvatarSeeder";
+import Avatar from "../entities/Avatar";
+import User from "../entities/User";
 
 async function generateAndSaveFixtures() {
   try {
     await dataSource.initialize();
     await dataSource.synchronize(true);
+    
+await dataSource.getRepository(Avatar).save(Avatars);
 
     const savedUsers = await dataSource
-      .getRepository("User")
+      .getRepository(User)
       .save(Users);
 
     console.info("Seeded users:", savedUsers);
--- server/src/seeders/data/UserSeeder.ts
+++ server/src/seeders/data/UserSeeder.ts
@@ -1,14 +1,13 @@
 const Users = [
-{
+ {
     id: 1,
     email: "admin@admin.com",
     first_name: "admin",
     last_name: "user",
-    hashedPassword:
-      "$2b$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36c6/Impeog4pJf0uHqvM5e",
-      birthdate: "1990-01-01",
-      avatar_id: 1,
-}
+    hashed_password: "$2b$10$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36c6/Impeog4pJf0uHqvM5e",
+    birthdate: new Date("1990-01-01"),
+    avatar: { id: 1 } 
+  }
 ];
 
 export { Users };
