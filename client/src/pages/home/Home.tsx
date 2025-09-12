bc11513718a2cab48f4f3fdfe9f92ce72f1d83e6
verocussi <verocussi@gmail.com>
2025-09-12 14:20:45 +0200

    refactor: remove Home dark mode and related logic from Home.module.scss and Home.tsx

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,27 +1,21 @@
 import { Link } from "react-router-dom";
 
-import { useMode } from "../../context/modeContext";
-
 import classes from "./Home.module.scss";
 
 import logo from "../../assets/img/logos_icons/logo.png";
-import logoDark from "../../assets/img/logos_icons/logo_dark.png";
 
 export default function Home() {
-  const { mode } = useMode();
-  const logoSrc = mode === "dark" ? logoDark : logo;
-
   return (
-    <section className={`${classes["home"]} ${classes[`home--${mode}`]}`}>
+    <section className={`${classes["home"]} ${classes["home--light"]}`}>
       {/* Header with Logo */}
       <header className={classes["home__header"]}>
         <div className={classes["home__header__content"]}>
           <img
-            src={logoSrc}
+            src={logo}
             alt="Footsy"
             className={classes["home__header__logo"]}
           />
-          <p className={`${classes["home__header__tagline"]} ${classes[`home__header__tagline--${mode}`]}`}>
+          <p className={`${classes["home__header__tagline"]} ${classes["home__header__tagline--light"]}`}>
             Green It Yourself
           </p>
         </div>
@@ -31,7 +25,7 @@ export default function Home() {
       <div className={classes["home__content"]}>
         <div className={classes["home__actions"]}>
           <Link to="/signup" className={classes["home__actions__link"]}>
-            <button className={`${classes["home__button"]} ${classes[`home__button--${mode}`]}`}>
+            <button className={`${classes["home__button"]} ${classes["home__button--light"]}`}>
               Commencer
             </button>
           </Link>
