e8fffeeb37e015c2d583cc3ab34b883312072b0a
AudeNec <aude.nec@gmail.com>
2025-09-18 16:09:38 +0200

    fix/activity resolver

--- server/src/entities/Category.ts
+++ server/src/entities/Category.ts
@@ -1,22 +1,26 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, OneToMany, ManyToOne } from "typeorm";
 import { Field, Int, ObjectType } from "type-graphql";
-import Type from "./Type";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 
+import Type from "./Type";
 
 @ObjectType()
 @Entity()
 export default class Category extends BaseEntity {
-
   @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
-
 
   @Field(() => String)
   @Column("varchar", { length: 50 })
   title: string;
 
   @Field(() => Type)
-  @ManyToOne(() => Type, type => type.categories, { nullable: false })
+  @ManyToOne(() => Type, (type) => type.categories, { nullable: true })
   types: Type[];
-} 
+}
--- server/src/resolvers/ActivityResolver.ts
+++ server/src/resolvers/ActivityResolver.ts
@@ -1,34 +1,61 @@
-import { Resolver, Query, Mutation, Arg, Int, Float, InputType, Field } from "type-graphql";
+import {
+  Arg,
+  Field,
+  Float,
+  InputType,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+} from "type-graphql";
+
 import Activity from "../entities/Activity";
 import Type from "../entities/Type";
 import User from "../entities/User";
-import { InteractionInput } from "./InteractionResolver";
 
 @InputType()
-export class ActivityInput {
-    @Field(() => Int)
-      id: number;
-    
-      @Field(() => String)
-      title: string;
-    
-      @Field(() => Float, { nullable: true })
-      quantity: number;
-    
-      @Field(() => Date)
-      date: Date;
-    
-      @Field(() => Float)
-      co2_equivalent: number;
-    
-      @Field(() => Int)
-      user_id: number;
-    
-      @Field(() => Int)
-      type_id: number;
-    
-      @Field(() => InteractionInput)
-      interactions: InteractionInput[];
+export class CreateActivityInput {
+  @Field(() => String)
+  title: string;
+
+  @Field(() => Float, { nullable: true })
+  quantity: number;
+
+  @Field(() => Date)
+  date: Date;
+
+  @Field(() => Float)
+  co2_equivalent: number;
+
+  @Field(() => Int)
+  user_id: number;
+
+  @Field(() => Int)
+  type_id: number;
+}
+
+@InputType()
+export class UpdateActivityInput {
+  @Field(() => Int)
+  id: number;
+
+  @Field(() => String, { nullable: true })
+  title?: string;
+
+  @Field(() => Float, { nullable: true })
+  quantity?: number;
+
+  @Field(() => Date, { nullable: true })
+  date?: Date;
+
+  @Field(() => Float, { nullable: true })
+  co2_equivalent?: number;
+
+  @Field(() => Int, { nullable: true })
+  user_id?: number;
+
+  @Field(() => Int, { nullable: true })
+  type_id?: number;
 }
 
 @Resolver()
@@ -45,9 +72,8 @@ export default class ActivityResolver {
 
   @Mutation(() => Activity)
   async createActivity(
-    @Arg("data", () => ActivityInput) data: ActivityInput
+    @Arg("data", () => CreateActivityInput) data: CreateActivityInput
   ): Promise<Activity> {
-
     const user = await User.findOne({ where: { id: data.user_id } });
     if (!user) throw new Error("User not found");
 
@@ -64,15 +90,23 @@ export default class ActivityResolver {
 
   @Mutation(() => Activity, { nullable: true })
   async updateActivity(
-    @Arg("data", () => ActivityInput) data: ActivityInput
+    @Arg("data", () => UpdateActivityInput) data: UpdateActivityInput
   ): Promise<Activity | null> {
     const activity = await Activity.findOne({ where: { id: data.id } });
     if (!activity) return null;
 
-    if (data.title !== undefined) { activity.title = data.title };
-    if (data.quantity !== undefined) { activity.quantity = data.quantity };
-    if (data.date !== undefined) { activity.date = new Date(data.date) };
-    if (data.co2_equivalent !== undefined) { activity.co2_equivalent = data.co2_equivalent };
+    if (data.title !== undefined) {
+      activity.title = data.title;
+    }
+    if (data.quantity !== undefined) {
+      activity.quantity = data.quantity;
+    }
+    if (data.date !== undefined) {
+      activity.date = new Date(data.date);
+    }
+    if (data.co2_equivalent !== undefined) {
+      activity.co2_equivalent = data.co2_equivalent;
+    }
 
     if (data.user_id !== undefined) {
       const user = await User.findOne({ where: { id: data.user_id } });
@@ -97,4 +131,4 @@ export default class ActivityResolver {
     await activity.remove();
     return true;
   }
-} 
+}
