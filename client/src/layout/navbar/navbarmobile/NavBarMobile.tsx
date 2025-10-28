b70996c7fe7e6e1c2a4e52c6bc17e724c0035b4e
Claire-Cellier <c.cellier@gmail.com>
2025-10-28 14:59:49 +0100

    06/feat/layout responsive (#28)
    * Feat: create FormLayout component and integrate related components
    
    * update folder organization
    
    * create navbar component associated with navbarmobile and navbardesktop
    
    * YES !
    
    * commit before the end of the day
    
    * Navbardesktop
    
    * navbarmobile
    
    * Parametrage header footer navbar reponsive

---
+++ client/src/layout/navbar/navbarmobile/NavBarMobile.tsx
@@ -0,0 +1,88 @@
+import { useNavigate } from "react-router-dom";
+
+import community from "../../../assets/img/logos_icons/community.png"; 
+import community_dark from "../../../assets/img/logos_icons/community_dark.png";
+import dashboard from "../../../assets/img/logos_icons/dashboard.png";
+import dashboard_dark from "../../../assets/img/logos_icons/dashboard_dark.png";
+import footprint from "../../../assets/img/logos_icons/footprint.png";
+import footprint_dark from "../../../assets/img/logos_icons/footprint_dark.png";
+import history from "../../../assets/img/logos_icons/history.png";
+import history_dark from "../../../assets/img/logos_icons/history_dark.png";
+import information from "../../../assets/img/logos_icons/information.png";
+import information_dark from "../../../assets/img/logos_icons/information_dark.png";
+
+import classes from "./NavBarMobile.module.scss";
+
+interface NavBarMobileProps {
+  mode: string;
+}
+
+export default function NavBarMobile({ mode }: NavBarMobileProps) {
+  const navigate = useNavigate();
+
+  const footprintIcon = mode === "dark" ? footprint_dark : footprint;
+  const dashboardIcon = mode === "dark" ? dashboard_dark : dashboard;
+  const historyIcon = mode === "dark" ? history_dark : history;
+  const communityIcon = mode === "dark" ? community_dark : community;
+  const infoIcon = mode === "dark" ? information_dark : information;
+
+  return (
+    <section className={classes.navbarmobile}>
+      <button
+        type="button"
+        onClick={() => navigate("/dashboard")}
+        className={classes.navbarmobile__button}
+      >
+        <img
+          src={dashboardIcon}
+          alt="dashboard-icon"
+          className={classes.navbarmobile__img}
+        />
+      </button>
+      <button
+        type="button"
+        onClick={() => navigate("/history")}
+        className={classes.navbarmobile__button}
+      >
+        <img
+          src={historyIcon}
+          alt="history-icon"
+          className={classes.navbarmobile__img}
+        />
+      </button>
+      <button
+        type="button"
+        onClick={() => navigate("/add-activity")}
+        className={classes.navbarmobile__button}
+      >
+        <img
+          src={footprintIcon}
+          alt="footprint"
+          className={classes.navbarmobile__footprint}
+        />
+      </button>
+      <button
+        type="button"
+        onClick={() => navigate("/community")}
+        className={classes.navbarmobile__button}
+      >
+        <img
+          src={communityIcon}
+          alt="community-icon"
+          className={classes.navbarmobile__img}
+        />
+      </button>
+      <button
+        type="button"
+        onClick={() => navigate("/information")}
+        className={classes.navbarmobile__button}
+      >
+        <img
+          src={infoIcon}
+          alt="information-icon"
+          className={classes.navbarmobile__img}
+        />
+      </button>
+    </section>
+  );
+}
